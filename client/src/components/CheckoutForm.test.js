6aff67eda54eb1f6140254c1dfc8b7e35bc4ec99
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-07-19 23:55:07 -0400

    changes

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import PlantList from "./PlantList";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render (<useForm/>)
+    
+});
 
 test("form shows success message on submit with form details", () => {});
